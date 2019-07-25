a66edc1715941e0e63f622970586e62d618b1e83
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-07-25 14:02:53 -0400

    first commit

---
+++ models/book.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define(
+    'Book',
+    {
+      title: DataTypes.STRING,
+      author: DataTypes.STRING,
+      genre: DataTypes.STRING,
+      year: DataTypes.INTEGER,
+    },
+    {},
+  );
+  Book.associate = function (models) {
+    // associations can be defined here
+  };
+  return Book;
+};
