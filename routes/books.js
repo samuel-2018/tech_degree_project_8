ea49c4239f02b458fda863c162cf459fbcf25c47
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-07-29 10:56:55 -0400

    added pagination and search routing

--- routes/books.js
+++ routes/books.js
@@ -2,20 +2,91 @@ const express = require('express');
 
 const router = express.Router();
 
+// Is this correct? Is there a way to access the instance already in the program? or is this not creating an instance? Does it create a second copy of anything?
+const Sequelize = require('sequelize');
+
+const { Op } = Sequelize;
+
 // provides access to database via Book
 const { Book } = require('../models');
 
-/* GET full list of books. */
+/* GET redirects to results page. */
 router.get('/', (req, res, next) => {
-  Book.findAll({ order: [['title', 'ASC']] })
-    .then((books) => {
-      res.render('books', { books, title: 'Books' });
-      // console.log(books);
+  res.redirect('/books/catalog/1');
+  // could it do ./results  ? doesn't seem to work
+});
+
+// I want '/' and 'results' to go to the below:
+
+/* GET full list of books. */
+router.get('/catalog/:page', (req, res, next) => {
+  Book.findAndCountAll({ order: [['title', 'ASC']], offset: 10 * (req.params.page - 1), limit: 10 })
+    .then((result) => {
+      const { count } = result;
+      const books = result.rows;
+
+      // Determines number of pages
+      // Will return whole number
+      const pages = count >= 10 ? Math.ceil(count / 10) : 1;
+
+      res.render('books', {
+        books,
+        title: 'Books',
+        pages,
+        activePage: parseInt(req.params.page, 10),
+        query: false,
+      });
     })
     .catch((error) => {
       res.send(500, error);
     });
 });
+
+/* GET redirects to search results. */
+router.get('/search', (req, res, next) => {
+  // Retrieves query.
+  const query = req.query.search;
+  // Redirects with query and page 1
+  res.redirect(`/books/search/${query}/1`);
+});
+
+/* GET search list of books. */
+router.get('/search/:query/:page', (req, res, next) => {
+  Book.findAndCountAll({
+    where: {
+      [Op.or]: [
+        { title: { [Op.substring]: req.params.query } },
+        { author: { [Op.substring]: req.params.query } },
+        { genre: { [Op.substring]: req.params.query } },
+        { year: { [Op.substring]: req.params.query } },
+      ],
+    },
+    order: [['title', 'ASC']],
+    offset: 10 * (req.params.page - 1),
+    limit: 10,
+  })
+    .then((result) => {
+      const { count } = result;
+      const books = result.rows;
+
+      // Determines number of pages
+      // Will return whole number
+      const pages = count >= 10 ? Math.ceil(count / 10) : 1;
+
+      res.render('books', {
+        books,
+        title: 'Books',
+        pages,
+        activePage: parseInt(req.params.page, 10),
+        query: req.params.query,
+      });
+    })
+    .catch((error) => {
+      res.send(500, error);
+    });
+});
+
+// both 'new's could be grouped under router.route('/new', ....
 
 /* GET create new book form. */
 router.get('/new', (req, res, next) => {
@@ -34,8 +105,12 @@ router.post('/new', (req, res, next) => {
     });
 });
 
+// both '/:id's could be grouped under router.route('/:id', ....
+
 /* GET book detail form (update book). */
 router.get('/:id', (req, res, next) => {
+  console.log("router.get('/:id' RAN");
+
   Book.findByPk(req.params.id)
     .then((book) => {
       res.render('books/update-book', { book, title: 'Update Book' });
