0f831ec8a7a66e46e759d137276d82ac45d74e23
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-07-30 20:21:01 -0400

    added Favicon

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// Favicon
+const favicon = require('serve-favicon');
+
+const app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
-
-const app = express();
 
 // view engine setup
 app.set('view engine', 'pug');
