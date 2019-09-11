386cd9749c6de22d35a99e4cb29a68c81ce87dab
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-09-10 20:10:21 -0400

    Trying to get to work with "Phusion Passenger": changed file names and file paths (Phusion uses app.js as the start file but express generator created www as the start file)

--- app.js
+++ app.js
@@ -1,53 +1,87 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-// Favicon
-const favicon = require('serve-favicon');
+#!/usr/bin/env node
 
-const app = express();
+/**
+ * Module dependencies.
+ */
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+var app = require("./appOriginal");
+var debug = require("debug")("myapp:server");
+var http = require("http");
+const sequelize = require("./models").sequelize;
+/**
+ * Get port from environment and store in Express.
+ */
 
-const indexRouter = require('./routes/index');
-const booksRouter = require('./routes/books');
+var port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-// view engine setup
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+/**
+ * Create HTTP server.
+ */
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(app);
 
-app.use('/', indexRouter);
-app.use('/books', booksRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+sequelize.sync().then(function() {
+  server.listen(port);
 });
+server.on("error", onError);
+server.on("listening", onListening);
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-  // render the error page
-  // res.status(err.status || 500);
-  if (err.status === 404) {
-    res.status(404);
-    console.log('404: Page not found');
-    res.render('books/page-not-found');
-  } else {
-    res.status(500);
-    res.render('error');
-    console.log('500: Something went wrong.');
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
   }
-});
 
-module.exports = app;
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
